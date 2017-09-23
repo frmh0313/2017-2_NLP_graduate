e486283454b30641cb35e7c13f91ab90e4c680f6
frmh0313 <rmh0313@gmail.com>
2017-09-23 11:09:52 +0900

    solved 20, 22

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -2,42 +2,48 @@ import nltk
 from nltk.corpus import brown
 from pprint import pprint
 
-brown_words = brown.words()
-#15
+# 15
 def occur_at_least_three():
     # brown_words = list(brown.words(fileids=['ca01']))
+    brown_words = brown.words()
     result = [word for (word, count) in dict(nltk.FreqDist(brown_words)).items() if count > 3]
     return result
 
+
 pprint(occur_at_least_three())
 
-#16
+# 16
 
-#17
+# 17
+
+
 '''
-TODO
-조교 문의 -> preprocess한 걸로 해야되는지, 아니면 그냥 brown corpus에서 바로 가져온 것으로 해야하는지 
+# TODO
+# 조교 문의 -> preprocess한 걸로 해야되는지, 아니면 그냥 brown corpus에서 바로 가져온 것으로 해야하는지
 '''
+
+
 # preprocessed
 def most_50_excluding_stopwords_preprocessed():
+    brown_words = brown.words()
     stopwords = nltk.corpus.stopwords.words('english')
     lowercased_words = [w.lower() for w in brown_words if w.isalpha()]
     fdist = nltk.FreqDist(word for word in lowercased_words if word not in stopwords)
     return fdist.most_common(50)
 '''
-def most_50_excluding_stopwords():
-    stopwords = nltk.corpus.stopwords.words('english')
-    fdist = nltk.FreqDist(word for word in brown_words if word.lower() not in stopwords)
-    return fdist.most_common(50)
+# def most_50_excluding_stopwords():
+#     stopwords = nltk.corpus.stopwords.words('english')
+#     fdist = nltk.FreqDist(word for word in brown_words if word.lower() not in stopwords)
+#     return fdist.most_common(50)
 '''
 # print(most_50_excluding_stopwords())
 pprint(most_50_excluding_stopwords_preprocessed())
 
 
-#18
+# 18
 def most_50_bigrams_without_stopwords():
     stopwords = nltk.corpus.stopwords.words('english')
-    bigrams = list(nltk.bigrams(brown))
+    bigrams = list(nltk.bigrams(brown.words()))
     bigrams_without_stopwords = [(b1, b2)
                                  for (b1, b2) in bigrams
                                  if b1.lower() not in stopwords
@@ -45,9 +51,32 @@ def most_50_bigrams_without_stopwords():
     fdist = nltk.FreqDist(bigrams_without_stopwords)
     return fdist.most_common(50)
 
+
 pprint(most_50_bigrams_without_stopwords())
 
-#19
+# 19
 
 
+# 20
+def word_freq(word, section):
+    corpus = brown.words(categories=section)
+    fdist = nltk.FreqDist(corpus)
+    return fdist[word]
 
+
+print("'new' appears %d times in the 'news' category of Brown corpus" % word_freq('new', 'news'))
+
+
+# string input?
+# 22
+def hedge(text):
+    l = text.split(' ')
+    for i in range(3, int(len(l)+len(l)/3)):
+        if (i-3)%4 == 0:
+            l.insert(i, 'like')
+    return ' '.join(l)
+
+
+text = "Define a function hedge(text) which processes a text and produces a new version with the word 'like' between every third word."
+
+print(hedge(text))
