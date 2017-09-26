f64322fb5b41d0f0380941e8adf56c217b2833c1
frmh0313 <rmh0313@gmail.com>
2017-09-26 16:10:50 +0900

    modified start_with_qu regex

--- hw1/2010-10225_hw1-2.py
+++ hw1/2010-10225_hw1-2.py
@@ -20,12 +20,10 @@ def pig_latin(file, out='./output.txt'):
     start_with_non_alpha = re.compile(r'^([\W_]+)(.*)')
     end_with_non_alpha = re.compile(r'(.*?)([\W_]+)$')
 
-    start_with_qu = re.compile(r'^([qQ][uU][^\W\dAEIOUYaeiouy_])(.*)')
-    #consonants_with_qu = re.compile(r'^([^\W\dAEIOUYaeiouy_]+[qQ][uU][^\W\dAEIOUYaeiouy_]*)(.*)')
+    start_with_qu = re.compile(r'^([qQ][uU])([AEIOUYaeiouy]+.*)')
     consonants_qu = re.compile(r'^([^\W\dAEIOUYaeiouy_]+[qQ][uU][^\W\dAEIOUYaeiouy_]*)(.*)')
     qu_consonants = re.compile(r'^([^\W\dAEIOUYaeiouy]*[qQ][uU][^\W\dAEIOUYaeiouy_]+)(.*)')
 
-    consonants_without_qu = re.compile(r'^([^\W\dAEIOUYaeiouy_]+)(.*)')
     start_with_y = re.compile(r'^([yY][^\W\dAEIOUYaeiouy_]*)(.*)')
 
     for word in words:
@@ -47,33 +45,17 @@ def pig_latin(file, out='./output.txt'):
             word = is_end_with_non_alpha.group(1)
 
         is_start_with_y = start_with_y.match(word)
-        is_start_with_consonants_without_qu = consonants_without_qu.match(word)
-        #is_start_with_consonants_with_qu = consonants_with_qu.match(word)
         is_start_with_consonants_qu = consonants_qu.match(word)
         is_start_with_qu_consonants = qu_consonants.match(word)
         is_start_with_qu = start_with_qu.match(word)
 
-
-        '''
-        if is_start_with_y: # y가 자음인 경우: y로 시작하는 경우
+        if is_start_with_y:  # y가 자음인 경우: y로 시작하는 경우
             w += is_start_with_y.group(2) + is_start_with_y.group(1)
-        elif is_start_with_qu: # qu로 시작하는 경우
-            w += is_start_with_qu.group(2) + is_start_with_qu.group(1)
-        elif is_start_with_consonants_with_qu: # 자음 + qu (+자음)인 경우
-            w += is_start_with_consonants_with_qu.group(2) + is_start_with_consonants_with_qu.group(1)
-        elif is_start_with_consonants_without_qu: # qu를 제외한 자음으로 시작하는 경우
-            w += is_start_with_consonants_without_qu.group(2) + is_start_with_consonants_without_qu.group(1)
-        else: # 모음으로 시작하는 경우
-            w += word
-        '''
-
-        if is_start_with_y: # y가 자음인 경우: y로 시작하는 경우
-            w += is_start_with_y.group(2) + is_start_with_y.group(1)
-        elif is_start_with_consonants_qu: # 자음 + qu (+ 자음)*인 경우
-            w += is_start_with_consonants_qu.group(2) + is_start_with_consonants_qu(1)
-        elif is_start_with_qu_consonants: # (자음)* + qu + 자음인 경우
-            w += is_start_with_qu_consonants.group(2) + is_start_with_qu_consonants(1)
-        elif is_start_with_qu: # qu + 모음인 경우
+        elif is_start_with_consonants_qu:  # 자음 + qu (+ 자음)*인 경우
+            w += is_start_with_consonants_qu.group(2) + is_start_with_consonants_qu.group(1)
+        elif is_start_with_qu_consonants:  # (자음)* + qu + 자음인 경우
+            w += is_start_with_qu_consonants.group(2) + is_start_with_qu_consonants.group(1)
+        elif is_start_with_qu:  # qu + 모음인 경우
             w += is_start_with_qu.group(2) + is_start_with_qu.group(1)
         else:  # 모음으로 시작하는 경우
             w += word
@@ -86,12 +68,15 @@ def pig_latin(file, out='./output.txt'):
     o.close()
 
 
-
-if __name__ == '__main__':
+def main():
     if len(sys.argv) == 2:
         pig_latin(sys.argv[1])
     elif len(sys.argv) == 3:
         pig_latin(sys.argv[1], out=sys.argv[2])
     else:
         print("Wrong number of arguments.")
+
+
+if __name__ == '__main__':
+    main()
 
