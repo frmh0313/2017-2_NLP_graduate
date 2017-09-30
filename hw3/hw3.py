0bcde63ae3773606eab8bddebcb205c11572489f
frmh0313 <rmh0313@gmail.com>
2017-10-01 02:40:25 +0900

    modified jamo_counter, add entropy

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,21 +1,15 @@
 import hangulDecoder
+import math
 
 INITIAL_JAMO = hangulDecoder.INITIAL_JAMO.values()
 MED_JAMO = hangulDecoder.MID_JAMO.values()
-FINAL_JAMO = [ u"ㄳ",
-               u"ㄵ",
-               u"ㄶ",
-               u"ㄺ",
-               u"ㄻ",
-               u"ㄼ",
-               u"ㄽ",
-               u"ㄾ",
-               u"ㄿ",
-               u"ㅀ",
-               u"ㅄ"]
+#deleted u"none" and the syllables included in INITIAL_JAMO
+FINAL_JAMO = [ u"ㄳ", u"ㄵ", u"ㄶ", u"ㄺ", u"ㄻ", u"ㄼ", u"ㄽ", u"ㄾ", u"ㄿ", u"ㅀ", u"ㅄ"]
+JAMO = []
+JAMO.extend(INITIAL_JAMO, MED_JAMO, FINAL_JAMO)
 
 
-def stringDecoder(string):
+def string_unigram_decoder(string):
     result = []
     for character in string:
         result.extend(hangulDecoder.decodeSyllable(character))
@@ -23,23 +17,21 @@ def stringDecoder(string):
 
 
 def jamo_counter(string):
-    decoded_string = stringDecoder(string)
-    initial_jamo_count = {}
-    med_jamo_count = {}
-    final_jamo_count = {}
-    for char in decoded_string:
-        if char in INITIAL_JAMO:
-            initial_jamo_count[char] = initial_jamo_count[char] + 1 if char in initial_jamo_count.keys() else 1
-        elif char in MED_JAMO:
-            med_jamo_count[char] = med_jamo_count[char] + 1 if char in med_jamo_count.keys() else 1
-        elif char in FINAL_JAMO:
-            final_jamo_count[char] = final_jamo_count[char] + 1 if char in final_jamo_count.keys() else 1
-
-    return initial_jamo_count, med_jamo_count, final_jamo_count
+    decoded_list = string_unigram_decoder(string)
+    total_count = len(decoded_list)
+    jamo_count = {}
+    for char in decoded_list:
+        jamo_count[char] = jamo_count[char] + 1 if char in jamo_count.keys() else 1
+    return total_count, jamo_count
 
 
+def entropy(total_count, jamo_count):
+    result = 0
+    for jamo in jamo_count:
+        probability = jamo_count[jamo]/total_count
+        result += -probability * math.log2(probability)
+    return result
 
 
-
-
+def cross_entropy(training_total_count, training_count, test_total_count, test_count):
 
