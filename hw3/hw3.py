ba730bfe121ff3edfaf3eabd83c9578bc6a97125
frmh0313 <rmh0313@gmail.com>
2017-10-01 03:11:20 +0900

    add bigram_generator, modified counter

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,8 +1,8 @@
-import hangulDecoder
+import hangul_decoder_modified
 import math
 
-INITIAL_JAMO = hangulDecoder.INITIAL_JAMO.values()
-MED_JAMO = hangulDecoder.MID_JAMO.values()
+INITIAL_JAMO = hangul_decoder_modified.INITIAL_JAMO.values()
+MED_JAMO = hangul_decoder_modified.MID_JAMO.values()
 #deleted u"none" and the syllables included in INITIAL_JAMO
 FINAL_JAMO = [ u"ㄳ", u"ㄵ", u"ㄶ", u"ㄺ", u"ㄻ", u"ㄼ", u"ㄽ", u"ㄾ", u"ㄿ", u"ㅀ", u"ㅄ"]
 JAMO = []
@@ -12,12 +12,27 @@ JAMO.extend(INITIAL_JAMO, MED_JAMO, FINAL_JAMO)
 def string_unigram_decoder(string):
     result = []
     for character in string:
-        result.extend(hangulDecoder.decodeSyllable(character))
+        result.extend(hangul_decoder_modified.decodeSyllable(character))
     return result
 
 
-def jamo_counter(string):
-    decoded_list = string_unigram_decoder(string)
+def bigram_generator(string):
+    unigrams = string_unigram_decoder(string)
+    bigrams = []
+    for i in range(len(unigrams) - 1):
+        if i == 0:
+            bigrams.append(('<s>', unigrams[i]))
+            bigrams.append((unigrams[i], unigrams[i+1]))
+        elif i == len(unigrams) -1 :
+            bigrams.append((unigrams[i], '</s>'))
+        else:
+            single_bigram = (unigrams[i], unigrams[i+1])
+            if single_bigram not in bigrams:
+                bigrams.append(single_bigram)
+    return bigrams
+
+
+def counter(decoded_list):
     total_count = len(decoded_list)
     jamo_count = {}
     for char in decoded_list:
@@ -34,4 +49,5 @@ def entropy(total_count, jamo_count):
 
 
 def cross_entropy(training_total_count, training_count, test_total_count, test_count):
+
 
