4bebc0a5d9555cf474cba45352259a3725296d3f
frmh0313 <rmh0313@gmail.com>
2017-10-01 13:26:16 +0900

    solved managing <UNK>

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -38,12 +38,15 @@ def bigram_generator(string):
     return bigrams
 
 
-def counter(decoded_list):
-    total_count = len(decoded_list)
-    jamo_count = {}
-    for char in decoded_list:
-        jamo_count[char] = jamo_count[char] + 1 if char in jamo_count.keys() else 1
-    return total_count, jamo_count
+def counter(test_decoded, training_decoded):
+    total_count = len(test_decoded)
+    counts = {}
+    for char in test_decoded:
+        if char in training_decoded:
+            counts[char] = counts[char] + 1 if char in counts.keys() else 1
+        else: # test set에는 있지만 training set에는 들어있지 않은 단어의 경우 '<UNK>'로 처리하여 test counter dict에 저장.
+            counts['<UNK>'] = counts['<UNK>'] + 1 if '<UNK>' in counts.keys() else 1
+    return total_count, counts
 
 
 def entropy(total_count, jamo_counts):
@@ -60,15 +63,23 @@ def cross_entropy(training_total_count, training_counts, test_total_count, test_
     test_probabilities = {key: float(test_counts[key])/test_total_count for key in test_counts.keys()}
 
     # key_list = training_counts.keys() # key list of training_counts and test_counts are same.
-    # when there are characters which is in test corpus, but not in training corpus/ or vice versa?
+    # TODO when there are characters which is in test corpus, but not in training corpus/ or vice versa?
+    """
+    @ Test corpus에는 들어있지만 training corpus에 들어있지 않은 경우 -> 그냥 0이 더해짐.
+    @ Training corpus에는 들어있지만 test corpus에는 들어있지 않은 경우 -> 
+    """
 
     for key in test_counts.keys():
-        result += -test_probabilities[key] * math.log2(training_probabilities[key])
+        if key in training_counts.keys():
+            result += -test_probabilities[key] * math.log2(training_probabilities[key])
+        else:
+            result += -test_probabilities['<UNK>'] * math.log2(training_probabilities[key])
 
     return result
 
 
 if __name__ == '__main__':
+    # 자소 모델
     training_raw = ""
     test_raw = ""
     hani_raw = ""
@@ -79,61 +90,70 @@ if __name__ == '__main__':
     with open('./hani.test.txt', 'r', encoding='utf8') as f:
         hani_raw = f.read()
 
-    # with open('./out.txt', 'w') as f:
-    training_unigram_decoded = unigram_decoder(training_raw)
-    print("training unigram decoded:", training_unigram_decoded)
-    training_unigram_counted = counter(training_unigram_decoded)
-    print("training unigram counted:", training_unigram_counted)
-    training_unigram_entropy = entropy(*training_unigram_counted)
-    print("training unigram entropy:", training_unigram_entropy)
-    training_unigram_cross_entropy = cross_entropy(*training_unigram_counted, *training_unigram_counted)
-    print("training unigram cross entropy:", training_unigram_entropy)
+    with open('./out.txt', 'w', encoding='utf8') as f:
+        print("training unigram")
+        training_unigram_decoded = unigram_decoder(training_raw)
+        f.write("training unigram decoded: " + str(training_unigram_decoded))
+        training_unigram_counted = counter(test_decoded=training_unigram_decoded, training_decoded=training_unigram_decoded)
+        f.write("training unigram counted: " + str(training_unigram_counted))
+        training_unigram_entropy = entropy(*training_unigram_counted)
+        f.write("training unigram entropy: " + str(training_unigram_entropy))
+        training_unigram_cross_entropy = cross_entropy(*training_unigram_counted, *training_unigram_counted)
+        f.write("training unigram cross entropy: " + str(training_unigram_entropy))
 
-    training_bigram_decoded = bigram_generator(training_raw)
-    print("training bigram decoded:", training_bigram_decoded)
-    training_bigram_counted = counter(training_bigram_decoded)
-    print("training bigram counted:", training_bigram_counted)
-    training_bigram_entropy = entropy(*training_bigram_counted)
-    print("training bigram entropy:", training_bigram_entropy)
-    training_bigram_cross_entropy = cross_entropy(*training_bigram_counted,
-                                                  *training_bigram_counted)
-    print("training bigram cross entropy:", training_bigram_cross_entropy)
+        print("training bigram")
+        training_bigram_decoded = bigram_generator(training_raw)
+        f.write("training bigram decoded: " + str(training_bigram_decoded))
+        training_bigram_counted = counter(test_decoded=training_bigram_decoded, training_decoded=training_bigram_decoded)
+        f.write("training bigram counted: " + str(training_bigram_counted))
+        training_bigram_entropy = entropy(*training_bigram_counted)
+        f.write("training bigram entropy: " + str(training_bigram_entropy))
+        training_bigram_cross_entropy = cross_entropy(*training_bigram_counted,
+                                                      *training_bigram_counted)
+        f.write("training bigram cross entropy: " + str(training_bigram_cross_entropy))
 
-    test_unigram_decoded = unigram_decoder(test_raw)
-    print("test unigram decoded:", test_unigram_decoded)
-    test_unigram_counted = counter(test_unigram_decoded)
-    print("test unigram counted:", test_unigram_counted)
-    test_unigram_entropy = entropy(*test_unigram_counted)
-    print("test unigram entropy:", test_unigram_entropy)
-    test_unigram_cross_entropy = cross_entropy(*training_unigram_counted,
-                                               *test_unigram_counted)
-    print("test unigram cross entropy:", test_unigram_cross_entropy)
+        print("test unigram")
+        test_unigram_decoded = unigram_decoder(test_raw)
+        f.write("test unigram decoded: " + str(test_unigram_decoded))
+        test_unigram_counted = counter(test_decoded=test_unigram_decoded, training_decoded=training_unigram_decoded)
+        f.write("test unigram counted: " + str(test_unigram_counted))
+        test_unigram_entropy = entropy(*test_unigram_counted)
+        f.write("test unigram entropy: " + str(test_unigram_entropy))
+        test_unigram_cross_entropy = cross_entropy(*training_unigram_counted,
+                                                   *test_unigram_counted)
+        f.write("test unigram cross entropy: " + str(test_unigram_cross_entropy))
 
-    test_bigram_decoded = bigram_generator(test_raw)
-    print("test bigram decoded:", test_bigram_decoded)
-    test_bigram_counted = counter(test_bigram_decoded)
-    print("test bigram counted:", test_bigram_counted)
-    test_bigram_entropy = entropy(*test_bigram_counted)
-    print("test bigram entropy:", test_bigram_entropy)
-    test_bigram_cross_entropy = cross_entropy(*training_bigram_counted,
-                                              *test_bigram_counted)
-    print("test bigram cross entropy:", test_bigram_cross_entropy)
+        print("test bigram")
+        test_bigram_decoded = bigram_generator(test_raw)
+        f.write("test bigram decoded: " + str(test_bigram_decoded))
+        test_bigram_counted = counter(test_decoded=test_bigram_decoded, training_decoded=training_bigram_decoded)
+        f.write("test bigram counted: " + str(test_bigram_counted))
+        test_bigram_entropy = entropy(*test_bigram_counted)
+        f.write("test bigram entropy: " + str(test_bigram_entropy))
+        test_bigram_cross_entropy = cross_entropy(*training_bigram_counted,
+                                                  *test_bigram_counted)
+        f.write("test bigram cross entropy: " + str(test_bigram_cross_entropy))
 
-    hani_test_unigram_decoded = unigram_decoder(hani_raw)
-    print("hani unigram decoded:", hani_test_unigram_decoded)
-    hani_test_unigram_counted = counter(hani_test_unigram_decoded)
-    print("hani unigram counted:", hani_test_unigram_counted)
-    hani_test_unigram_entropy = entropy(*hani_test_unigram_counted)
-    print("hani unigram entropy:", hani_test_unigram_entropy)
-    hani_test_unigram_cross_entropy = cross_entropy(*training_unigram_counted,
-                                                    *hani_test_unigram_counted)
-    print("hani unigram cross entropy:", hani_test_unigram_cross_entropy)
-    hani_test_bigram_decoded = bigram_generator(hani_raw)
-    print("hani bigram decoded:", hani_test_bigram_decoded)
-    hani_test_bigram_counted = counter(hani_test_bigram_decoded)
-    print("hani bigram counted:", hani_test_bigram_counted)
-    hani_test_bigram_entropy = entropy(*hani_test_unigram_counted)
-    print("hani bigram entropy:", hani_test_bigram_entropy)
-    hani_test_bigram_cross_entropy = cross_entropy(*training_bigram_counted,
-                                                   *hani_test_bigram_counted)
-    print("hani bigram cross entropy;", hani_test_bigram_cross_entropy)
+        print("hani test unigram")
+        hani_test_unigram_decoded = unigram_decoder(hani_raw)
+        f.write("hani unigram decoded: " + str(hani_test_unigram_decoded))
+        hani_test_unigram_counted = counter(test_decoded=hani_test_unigram_decoded, training_decoded=training_unigram_decoded)
+        f.write("hani unigram counted: " + str(hani_test_unigram_counted))
+        hani_test_unigram_entropy = entropy(*hani_test_unigram_counted)
+        f.write("hani unigram entropy: " + str(hani_test_unigram_entropy))
+        hani_test_unigram_cross_entropy = cross_entropy(*training_unigram_counted,
+                                                        *hani_test_unigram_counted)
+        f.write("hani unigram cross entropy: " + str(hani_test_unigram_cross_entropy))
+        hani_test_bigram_decoded = bigram_generator(hani_raw)
+        print("hani test bigram")
+        f.write("hani bigram decoded: " + str(hani_test_bigram_decoded))
+        hani_test_bigram_counted = counter(test_decoded=hani_test_bigram_decoded, training_decoded=training_bigram_decoded)
+        f.write("hani bigram counted: " + str(hani_test_bigram_counted))
+        hani_test_bigram_entropy = entropy(*hani_test_unigram_counted)
+        f.write("hani bigram entropy: " + str(hani_test_bigram_entropy))
+        hani_test_bigram_cross_entropy = cross_entropy(*training_bigram_counted,
+                                                       *hani_test_bigram_counted)
+        f.write("hani bigram cross entropy: " + str(hani_test_bigram_cross_entropy))
+
+    # 음절별 모델
+
